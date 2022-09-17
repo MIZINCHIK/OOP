1ba6ea8182721167b3215f57ebbde2222745ee61
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-09-17 23:03:25 +0700

    Added JavaDoc to the tests

--- Task_1_1_1/src/test/java/io/github/mizinchik/HeapSortTest.java
+++ Task_1_1_1/src/test/java/io/github/mizinchik/HeapSortTest.java
@@ -2,14 +2,21 @@ package io.github.mizinchik;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.util.Arrays;
+import java.util.Random;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Random;
-
+/**
+ * The ultimate heapsort-testing class.
+ * Provides all the tests needed to submit the lab.
+ * @author MIZINCHIK
+ */
 public class HeapSortTest {
 
+    /**
+     * Checks correctness on an empty array.
+     */
     @Test
     @DisplayName("Empty array")
     void testEmpty() {
@@ -20,6 +27,9 @@ public class HeapSortTest {
             "Sorting an empty array went wrong");
     }
 
+    /**
+     * Checks correctness on a small array with a mixed order.
+     */
     @Test
     @DisplayName("Small random array")
     void testSmallRandom() {
@@ -30,6 +40,9 @@ public class HeapSortTest {
             "Sorting a small random array went wrong");
     }
 
+    /**
+     * Checks correctness on a small sorted array.
+     */
     @Test
     @DisplayName("Sorted array")
     void testSorted() {
@@ -40,6 +53,9 @@ public class HeapSortTest {
             "Sorting a sorted array went wrong");
     }
 
+    /**
+     * Checks correctness on a small array with a reversed order.
+     */
     @Test
     @DisplayName("Reversed array")
     void testReversed() {
@@ -47,15 +63,18 @@ public class HeapSortTest {
         int [] array = new int [] {5, 4, 3, 2, 1};
         HeapSort.heapSort(array);
         assertArrayEquals(referenceArray, array,
-        "Sorting a reversed array went wrong");
+            "Sorting a reversed array went wrong");
     }
 
+    /**
+     * Checks correctness on a large random array.
+     */
     @Test
     @DisplayName("Large array")
     void testLargeRandom() {
         Random random = new Random();
         int [] array = new int[100000];
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt();
         }
         int [] sortedArray = new int[100000];
