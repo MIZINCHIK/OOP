3720c523d434902637224fb2dde0d54a54a2ab07
MIZINCHIK <MIZINCHIK@users.noreply.github.com>
2022-10-23 14:15:43 +0000

    Deploying to gh-pages from @ MIZINCHIK/OOP@7484dcbdb45322bda2054580d8a2650ee902d4f7 🚀

---
+++ Task_1_2_3/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.github.mizinchik","l":"Edge"},{"p":"io.github.mizinchik","l":"EdgeImpl"},{"p":"io.github.mizinchik","l":"Graph"},{"p":"io.github.mizinchik","l":"GraphImpl"},{"p":"io.github.mizinchik","l":"GraphInteger"},{"p":"io.github.mizinchik","l":"Vertex"},{"p":"io.github.mizinchik","l":"VertexImpl"}]
