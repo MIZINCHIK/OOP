d7d5570214977c987e1c4cda8e8789382b543cf3
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-11-13 13:48:27 +0700

    added specific tests

--- Task_1_3_1/src/test/java/io/github/mizinchik/SubstringFinderTest.java
+++ Task_1_3_1/src/test/java/io/github/mizinchik/SubstringFinderTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SubstringFinderTest {
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -48,5 +48,25 @@ public class SubstringFinderTest {
         assertEquals("0 3 6 9 12 15 18 21 24 \n", out.toString());
         finder.eatReaderAndSubstring("./src/main/resources/CommonTest.txt", "aab");
         assertEquals("0 3 6 9 12 15 18 21 24 \n0 3 6 9 12 15 18 21 24 27 \n", out.toString());
+    }
+
+    @Test
+    @DisplayName("Minor methods test")
+    void testMinor() throws FileNotFoundException {
+        var reader = new InputStreamReader(new FileInputStream("./src/main/resources/RandomTest.txt"), StandardCharsets.UTF_8);
+        var finder = new SubstringFinderImpl(reader, "bet");
+        assertEquals("98 12349 16565 \n", out.toString());
+        reader = new InputStreamReader(new FileInputStream("./src/main/resources/RandomTest.txt"), StandardCharsets.UTF_8);
+        finder.eatReaderAndSubstring(reader, "sdasd");
+        assertEquals("98 12349 16565 \n\n", out.toString());
+        reader = new InputStreamReader(new FileInputStream("./src/main/resources/RandomTest.txt"), StandardCharsets.UTF_8);
+        finder.eatReader(reader);
+        assertEquals("98 12349 16565 \n\n\n", out.toString());
+        reader = new InputStreamReader(new FileInputStream("./src/main/resources/PieTest.txt"), StandardCharsets.UTF_8);
+        assertThrows(UnsupportedOperationException.class, () -> {
+            finder.eatReaderAndSubstring((Reader) null, "a");
+        });
+        finder.eatReaderAndSubstring(reader, "");
+        assertEquals("98 12349 16565 \n\n\n0 1 2 3 4 5 6 7 8 9 10 11 12 \n", out.toString());
     }
 }
