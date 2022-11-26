1fe53d8a72eb964b46378f22fc9c644f1ed54cfe
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-11-26 21:32:43 +0700

    write grade classes javadocs

--- Task_1_4_1/src/main/java/io/github/mizinchik/GradeCredit.java
+++ Task_1_4_1/src/main/java/io/github/mizinchik/GradeCredit.java
@@ -1,9 +1,20 @@
 package io.github.mizinchik;
 
+/**
+ * Represents a grade which is represented by
+ * a boolean value. True means the form of evaluation was passed.
+ *
+ * @author MIZINCHIK
+ */
 public class GradeCredit implements Grade {
     private Boolean credit;
     private Integer mark;
 
+    /**
+     * Constructs a grade from a boolean value of it.
+     *
+     * @param credit to put as a grade
+     */
     public GradeCredit(Boolean credit) {
         if (credit == null) {
             this.credit = null;
@@ -18,16 +29,33 @@ public class GradeCredit implements Grade {
         }
     }
 
+    /**
+     * Get the grade in its genuine format.
+     * Null means the grade hasn't been put yet.
+     *
+     * @return true if passed
+     */
     @Override
     public Boolean getCredit() {
         return credit;
     }
 
+    /**
+     * Get the grade in a numerical format.
+     * Null means the grade hasn't been put yet.
+     *
+     * @return 5 if passed, 2 if not
+     */
     @Override
     public Integer getMark() {
         return mark;
     }
 
+    /**
+     * Set a new grade value.
+     *
+     * @param mark to set
+     */
     @Override
     public void updateGrade(Integer mark) {
         if (mark == null) {
