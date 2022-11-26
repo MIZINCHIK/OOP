1a9664cf04cea3dfa363af014401a61f061c3dd0
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-11-26 21:12:31 +0700

    wite javadoc for grade book class

--- Task_1_4_1/src/main/java/io/github/mizinchik/GradeBookImpl.java
+++ Task_1_4_1/src/main/java/io/github/mizinchik/GradeBookImpl.java
@@ -4,6 +4,12 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+/**
+ * Represents a grade book for
+ * the Novosibirsk State University.
+ *
+ * @author MIZINCHIK
+ */
 public class GradeBookImpl implements GradeBook {
     private Double GPA;
     private Integer diplomaGrade;
@@ -11,6 +17,14 @@ public class GradeBookImpl implements GradeBook {
     private final List<Term> terms;
     private List<Subject> markedSubjects;
 
+    /**
+     * Constructs a grade book from terms and a thesis grade.
+     *
+     * @param diplomaGrade if known
+     * @param termNumber which term you a currently in
+     * @param terms to get a diploma
+     * @throws IllegalArgumentException there are absent terms
+     */
     public GradeBookImpl(Integer diplomaGrade, int termNumber,
                          List<Term> terms) throws IllegalArgumentException {
         if (terms == null || termNumber == 0 || terms.contains(null)) {
@@ -23,47 +37,103 @@ public class GradeBookImpl implements GradeBook {
         computeGPA();
     }
 
+    /**
+     * Get a grade point average for all
+     * the subjects in the previous terms.
+     * Computes it each time it's called due to the
+     * presence of the ability to change the state of grades
+     * individually without notifying the parent.
+     *
+     * @return GPA
+     */
     @Override
     public Double getGPA() {
         computeGPA();
         return GPA;
     }
 
+    /**
+     * Get a grade of the thesis.
+     *
+     * @return diploma grade
+     */
     @Override
     public Integer getDiplomaGrade() {
         return diplomaGrade;
     }
 
+    /**
+     * Set the thesis' grade.
+     *
+     * @param grade of the diploma
+     */
     @Override
     public void setDiplomaGrade(Integer grade) {
         diplomaGrade = grade;
     }
 
+    /**
+     * Get the grade of a subject.
+     *
+     * @param subject which you seek for
+     * @param term in which the subject was evaluated
+     * @return the grade
+     */
     @Override
     public Grade getSubjectGrade(String subject, int term) {
         return terms.get(term - 1).getGrade(subject);
     }
 
+    /**
+     * Set the grade of a subject.
+     *
+     * @param subject which you seek for
+     * @param term in which the subject was evaluated
+     * @param grade of the evaluation
+     */
     @Override
     public void setSubjectGrade(String subject, int term, Integer grade) {
         terms.get(term - 1).updateGrade(subject, grade);
     }
 
+    /**
+     * Get the name of a lecturer on the subject.
+     *
+     * @param subject which you seek for
+     * @param term in which the subject was evaluated
+     * @return the lecturer's name
+     */
     @Override
     public String getLecturer(String subject, int term) {
         return terms.get(term - 1).getLecturer(subject);
     }
 
+    /**
+     * Get the amount of terms to complete to get
+     * a diploma.
+     *
+     * @return the amount of terms
+     */
     @Override
     public int getTermsAmount() {
         return terms.size();
     }
 
+    /**
+     * Get the number of the current term.
+     *
+     * @return the order number
+     */
     @Override
     public int getTermNumber() {
         return termNumber;
     }
 
+    /**
+     * Get the current term.
+     *
+     * @return the current term
+     */
     @Override
     public Term getCurrentTerm() {
         if (termNumber < terms.size()) {
@@ -73,28 +143,57 @@ public class GradeBookImpl implements GradeBook {
         }
     }
 
+    /**
+     * Changes the current term to the next one.
+     */
     @Override
     public void goNextTerm() {
         termNumber++;
         setMarkedSubjects();
     }
 
+    /**
+     * Get the list of all the terms required to
+     * get a diploma.
+     *
+     * @return list of terms
+     */
     @Override
     public List<Term> getTerms() {
         return terms;
     }
 
+
+    /**
+     * Set the list of all the subject that took place in
+     * all the previous terms.
+     */
     private void setMarkedSubjects() {
         markedSubjects = terms.subList(0, termNumber - 1).stream()
                 .flatMap(term -> term.getSubjects().stream())
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Get all the subjects which classes where conducted
+     * in the previous terms. And thus they are already
+     * necessarily evaluated.
+     *
+     * @return marked subjects
+     */
     @Override
     public List<Subject> getMarkedSubjects() {
         return markedSubjects;
     }
 
+    /**
+     * Check if it's still possible to graduate with honors.
+     * Graduation with honors is possible if no grades under 4 points
+     * were received, if the GPA is 4.75 or above and if the thesis
+     * is evaluated as excellent.
+     *
+     * @return true if honored graduation is possible
+     */
     @Override
     public boolean possibleHonoredGraduation() {
         return ((terms.stream().flatMap(term -> term.getSubjects().stream())
@@ -104,6 +203,13 @@ public class GradeBookImpl implements GradeBook {
                 && (diplomaGrade == null || diplomaGrade == 5));
     }
 
+    /**
+     * Check if you were granted with an extra stipend.
+     * You are granted with it if your previous term was completed
+     * with excellent marks only.
+     *
+     * @return true if you are receiving an extra stipend
+     */
     @Override
     public boolean receiveExtraStipend() {
         if (termNumber == 1) {
@@ -112,6 +218,10 @@ public class GradeBookImpl implements GradeBook {
         return terms.get(termNumber - 2).getGPA() == 5.0;
     }
 
+    /**
+     * Computes the GPA.
+     * If you have just started your education it is set to zero.
+     */
     private void computeGPA() {
         if (termNumber == 1) {
             GPA = (double) 0;
