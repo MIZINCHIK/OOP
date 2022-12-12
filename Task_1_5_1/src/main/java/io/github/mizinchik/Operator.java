3a0e13445fef841d09ce044af424ab36bbdc67da
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-12-12 10:48:33 +0700

    fix reviewdog

--- Task_1_5_1/src/main/java/io/github/mizinchik/Operator.java
+++ Task_1_5_1/src/main/java/io/github/mizinchik/Operator.java
@@ -1,6 +1,24 @@
 package io.github.mizinchik;
 
+/**
+ * Class for operators working on up to two
+ * Double arguments and returning Double on apply.
+ *
+ * @author MIZINCHIK
+ */
 public interface Operator {
+    /**
+     * Checks if the operator takes 2 arguments.
+     *
+     * @return if the operator is binary
+     */
     boolean isBinary();
+
+    /**
+     * Apply the operator to the operands.
+     *
+     * @param args operands
+     * @return value of the preformed operation
+     */
     Double apply(Double... args);
 }
