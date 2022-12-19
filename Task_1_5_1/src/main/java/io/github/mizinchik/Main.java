58a7b655aa6f826df14f204fb79e8120d4a2b12d
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-12-19 09:44:44 +0700

    Add appropriate method for calculations

--- Task_1_5_1/src/main/java/io/github/mizinchik/Main.java
+++ Task_1_5_1/src/main/java/io/github/mizinchik/Main.java
@@ -1,9 +1,10 @@
 package io.github.mizinchik;
 
-import java.util.EmptyStackException;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import static io.github.mizinchik.FunctionFactory.createFunction;
+import static io.github.mizinchik.Calculator.calculate;
 
 /**
  * Command-line calculator.
@@ -15,35 +16,18 @@ public class Main {
      * Executes a calculator. Prints the result of the calculation
      * to stdout.
      *
-     * @param args of the command line
      * @throws IllegalArgumentException if not enough operands
      */
-    public static void main(String[] args)
+    public static void main()
             throws IllegalArgumentException {
-        Stack<Double> numbers = new Stack<>();
-        for (int lastIndex = args.length - 1; lastIndex >= 0; lastIndex--) {
-            try {
-                numbers.push(Double.parseDouble(args[lastIndex]));
-            } catch (NumberFormatException e) {
-                try {
-                    Operator function = createFunction(args[lastIndex]);
-                    if (function.isBinary()) {
-                        Double firstArg = numbers.pop();
-                        Double secondArg = numbers.pop();
-                        numbers.push(function.apply(firstArg, secondArg));
-                    } else {
-                        Double arg = numbers.pop();
-                        numbers.push(function.apply(arg));
-                    }
-                } catch (EmptyStackException j) {
-                    throw new IllegalArgumentException("Not enough operands");
-                }
-            }
-        }
-        if (numbers.size() == 1) {
-            System.out.println(numbers.pop());
-        } else {
-            throw new IllegalArgumentException("Not enough operators");
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in))) {
+            String expressionRaw = reader.readLine();
+            String[] expression = expressionRaw.split(" ");
+            Double result = calculate(expression);
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
