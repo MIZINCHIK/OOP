39b908da8417411bc10f411cae171920417607e5
MIZINCHIK <MIZINCHIK@users.noreply.github.com>
2022-12-25 06:14:14 +0000

    Deploying to gh-pages from @ MIZINCHIK/OOP@d8b338052f59ea6944c0ff3950df212b22e5ea29 🚀

--- Task_1_5_2/type-search-index.js
+++ Task_1_5_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.github.mizinchik","l":"BookKeeper"},{"p":"io.github.mizinchik","l":"BookKeeperImpl"},{"p":"io.github.mizinchik","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.github.mizinchik","l":"BookKeeper"},{"p":"io.github.mizinchik","l":"Journal"}]
