a569eff7bc16f31272b1adbeef1ce5728e462c77
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-12-25 13:13:25 +0700

    Remake parsing args logic

--- Task_1_5_2/src/main/java/io/github/mizinchik/Journal.java
+++ Task_1_5_2/src/main/java/io/github/mizinchik/Journal.java
@@ -1,13 +1,32 @@
 package io.github.mizinchik;
+
+import static io.github.mizinchik.BookKeeper.addRecord;
+import static io.github.mizinchik.BookKeeper.removeGiven;
+import static io.github.mizinchik.BookKeeper.print;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-class Journal {
-    @Option(names = "-add", arity = "2", description = "add a new record")
-    String[] add;
+@Command(name = "Journal", mixinStandardHelpOptions = true)
+public class Journal {
+    @Command(name = "add", description = "Add a new record")
+    void add(@Option(names = "-add", arity = "2") String[] add) {
+        addRecord(add[0], add[1]);
+    }
 
-    @Option(names = "-rm", arity = "1..", description = "remove given records")
-    String[] names;
+    @Command(name = "remove", description = "Remove given records")
+    void remove(@Option(names = "-rm", arity = "1..") String[] names) {
+        removeGiven(names);
+    }
 
-    @Option(names = "-show", arity = "1..", description = "one or more files to archive")
-    String[] show;
+    @Command(name = "show", description = "Show records")
+    void show(@Option(names = "-show", arity = "0..") String[] show) {
+        print(show);
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Journal()).execute(args);
+        System.exit(exitCode);
+    }
 }
