a1be0bfec7c4e5886c4b9fb1071dcfa1fd9e07e5
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-12-28 16:50:53 +0700

    Add test

--- Task_1_5_2/src/test/java/io/github/mizinchik/JournalTest.java
+++ Task_1_5_2/src/test/java/io/github/mizinchik/JournalTest.java
@@ -1,7 +1,58 @@
 package io.github.mizinchik;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class JournalTest {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private final SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    /**
+     * Changes standard output stream before each method.
+     */
+    @BeforeEach
+    public void setUpOut() {
+        System.setOut(new PrintStream(out));
+    }
+
+    /**
+     * Restores standard output system after each method.
+     */
+    @AfterEach
+    public void restoreOut() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    @DisplayName("Reference test")
+    void testReference() {
+        Journal.main(new String[]{"remove"});
+        var date = new Date();
+        Journal.main(new String[]{"add", "Моя заметка", "Очень важная заметка"});
+        Journal.main(new String[]{"show"});
+        assertEquals(format.format(date) + " Моя заметка Очень важная заметка\r\n", out.toString());
+        Journal.main(new String[]{"show", format.format(date), format.format(date), "Моя"});
+        assertEquals(format.format(date) + " Моя заметка Очень важная заметка\r\n"
+                + format.format(date) + " Моя заметка Очень важная заметка\r\n", out.toString());
+        Journal.main(new String[]{"remove", "Моя заметка"});
+        Journal.main(new String[]{"show"});
+        assertEquals(format.format(date) + " Моя заметка Очень важная заметка\r\n"
+                + format.format(date) + " Моя заметка Очень важная заметка\r\n", out.toString());
+
+    }
+
+    @Test
+    @DisplayName("Complete test")
+    void testComplete() {
+
     }
 }
