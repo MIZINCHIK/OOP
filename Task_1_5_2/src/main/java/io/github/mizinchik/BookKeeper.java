9541d122cb8f0166e17cf60f68f80f2a00696686
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-12-28 12:34:08 +0700

    Implement essential methods

--- Task_1_5_2/src/main/java/io/github/mizinchik/BookKeeper.java
+++ Task_1_5_2/src/main/java/io/github/mizinchik/BookKeeper.java
@@ -6,12 +6,15 @@ import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
 import java.lang.reflect.Type;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 public interface BookKeeper {
     static String fileName = "bookkeeper.json";
+    static SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm");
 
     class Note {
         String date;
@@ -25,15 +28,18 @@ public interface BookKeeper {
         }
     }
 
-    static void addRecord(String recordName, String recordContents) {
-        var format = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+    static void addRecord(String recordName, String recordContents) throws RuntimeException {
         var date = new Date();
         var file = new File(fileName);
         if (file.exists()) {
-            try (var reader = new FileReader(file)) {
+            try (var reader = new FileReader(file);
+            var writer = new FileWriter(file)) {
                 Type type = new TypeToken<ArrayList<Note>>(){}.getType();
                 Gson gson = new GsonBuilder().create();
                 ArrayList<Note> list = gson.fromJson(reader, type);
+                var newNote = new Note(format.format(date), recordName, recordContents);
+                list.add(newNote);
+                gson.toJson(list, writer);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -47,17 +53,68 @@ public interface BookKeeper {
         }
     }
 
-    static void removeGiven(String[] names) {
-        for (String name : names) {
-
+    static void removeGiven(String[] names) throws RuntimeException {
+        var file = new File(fileName);
+        if (file.exists()) {
+            try (var reader = new FileReader(file);
+            var writer = new FileWriter(file)) {
+                Type type = new TypeToken<ArrayList<Note>>(){}.getType();
+                Gson gson = new GsonBuilder().create();
+                ArrayList<Note> list = gson.fromJson(reader, type);
+                list.removeIf(note -> Arrays.asList(names).contains(note.head));
+                gson.toJson(list, writer);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    static void print(String[] args) {
-
+    static void print(String[] args) throws IllegalArgumentException {
+        var file = new File(fileName);
+        if (file.exists()) {
+            try (var reader = new FileReader(file)) {
+                Type type = new TypeToken<ArrayList<Note>>(){}.getType();
+                Gson gson = new GsonBuilder().create();
+                ArrayList<Note> list = gson.fromJson(reader, type);
+                var position = new ParsePosition(0);
+                Date dateFrom = format.parse(args[0], position);
+                position.setIndex(0);
+                Date dateTo = format.parse(args[1], position);
+                if (dateFrom == null || dateTo == null) {
+                    throw new IllegalArgumentException("Incorrect date");
+                }
+                if (!dateTo.before(dateFrom)) {
+                    position.setIndex(0);
+                    for (Note note : list) {
+                        Date noteDate = format.parse(note.date, position);
+                        if (noteDate.after(dateFrom) && noteDate.before(dateTo)) {
+                            for (int i = 2; i < args.length; i++) {
+                                if (note.head.contains(args[i])) {
+                                    System.out.println(note.date + note.head + note.body);
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     static void printAll() {
-
+        var file = new File(fileName);
+        if (file.exists()) {
+            try (var reader = new FileReader(file)) {
+                Type type = new TypeToken<ArrayList<Note>>(){}.getType();
+                Gson gson = new GsonBuilder().create();
+                ArrayList<Note> list = gson.fromJson(reader, type);
+                for (Note note : list) {
+                    System.out.println(note.date + note.head + note.body);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
