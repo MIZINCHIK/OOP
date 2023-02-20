90463b69fc53beb0a49eaad5e3757346bb15ff59
MIZINCHIK <MIZINCHIK@users.noreply.github.com>
2023-02-20 04:05:55 +0000

    Deploying to gh-pages from @ MIZINCHIK/OOP@41e8de48be3b0b1d80475fc2403342c0c24e499e 🚀

--- Task_2_1_1/type-search-index.js
+++ Task_2_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.github.mizinchik","l":"CompositeArrayConsecutive"},{"p":"io.github.mizinchik","l":"CompositeArrayInterface"},{"p":"io.github.mizinchik","l":"CompositeArrayThread"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.github.mizinchik","l":"CompositeArrayConsecutive"},{"p":"io.github.mizinchik","l":"CompositeArrayInterface"},{"p":"io.github.mizinchik","l":"CompositeArrayStream"},{"p":"io.github.mizinchik","l":"CompositeArrayThread"}]
