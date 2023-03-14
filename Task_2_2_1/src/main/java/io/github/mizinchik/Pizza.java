1d787a64040d4817b421bec7b0ee66f86b22f071
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2023-03-14 20:13:54 +0700

    Convert record to class for a time being

--- Task_2_2_1/src/main/java/io/github/mizinchik/Pizza.java
+++ Task_2_2_1/src/main/java/io/github/mizinchik/Pizza.java
@@ -1,4 +1,54 @@
 package io.github.mizinchik;
 
-public record Pizza(int amount, int time, boolean ready) implements Order {
+import java.util.Objects;
+
+public final class Pizza implements Order {
+    private final int amount;
+    private final int time;
+    private final boolean ready;
+
+    public Pizza(int amount, int time, boolean ready) {
+        this.amount = amount;
+        this.time = time;
+        this.ready = ready;
+    }
+
+    @Override
+    public int amount() {
+        return amount;
+    }
+
+    @Override
+    public int time() {
+        return time;
+    }
+
+    @Override
+    public boolean ready() {
+        return ready;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Pizza) obj;
+        return this.amount == that.amount &&
+                this.time == that.time &&
+                this.ready == that.ready;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, time, ready);
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza[" +
+                "amount=" + amount + ", " +
+                "time=" + time + ", " +
+                "ready=" + ready + ']';
+    }
+
 }
