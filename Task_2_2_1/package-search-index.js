c6eb8479aa8ac533ba3386a5c29a6e7b911a7663
MIZINCHIK <MIZINCHIK@users.noreply.github.com>
2023-03-27 17:12:07 +0000

    Deploying to gh-pages from @ MIZINCHIK/OOP@1563a7c7188f1c6039b225cdb144a3080df35f7b 🚀

--- Task_2_2_1/package-search-index.js
+++ Task_2_2_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.mizinchik.Json"},{"l":"io.github.mizinchik.PizzaJoint"},{"l":"io.github.mizinchik.ProducerConsumer"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.mizinchik.PizzaJoint"},{"l":"io.github.mizinchik.PizzaJoint.Json"},{"l":"io.github.mizinchik.ProducerConsumer"}];updateSearchResults();
