f3538313ab113cc8329a913fc278743b5702ed46
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2023-03-28 00:10:56 +0700

    Add javadoc

--- Task_2_2_1/src/main/java/io/github/mizinchik/ProducerConsumer/Producer.java
+++ Task_2_2_1/src/main/java/io/github/mizinchik/ProducerConsumer/Producer.java
@@ -1,6 +1,20 @@
 package io.github.mizinchik.ProducerConsumer;
 
+/**
+ * Produces products.
+ *
+ * @author MIZINCHIK
+ */
 public interface Producer {
+    /**
+     * Start producing.
+     */
     void produce();
+
+    /**
+     * Get the id.
+     *
+     * @return identifier
+     */
     int id();
 }
--- Task_2_2_1/src/main/java/io/github/mizinchik/ProducerConsumer/WareHouse.java
+++ Task_2_2_1/src/main/java/io/github/mizinchik/ProducerConsumer/WareHouse.java
@@ -3,11 +3,54 @@ package io.github.mizinchik.ProducerConsumer;
 import io.github.mizinchik.PizzaJoint.Locks;
 import java.util.List;
 
+/**
+ * WareHouse where producers put
+ * and deliverers take ready orders.
+ *
+ * @author MIZINCHIK
+ * @param <T> Product
+ */
 public interface WareHouse<T extends Order> {
+    /**
+     * Add an order to the stock.
+     *
+     * @param order that's been produced
+     */
     void addWare(T order);
+
+    /**
+     * Hand the orders for delivery.
+     *
+     * @param capacity max amount of orders to hand
+     * @return list of orders
+     */
     List<T> giveOrder(int capacity);
+
+    /**
+     * Check if the storage is full.
+     *
+     * @return true if stock is full
+     */
     boolean isFull();
+
+    /**
+     * Check if the storage is empty.
+     *
+     * @return true if empty
+     */
     boolean isEmpty();
+
+    /**
+     * Get locks for producers and consumers
+     *
+     * @return Lock shared between every entity in the pizzeria.
+     */
     Locks locks();
+
+    /**
+     * Check if there are still cooks on duty.
+     *
+     * @return true if the storage can still be filled
+     */
     boolean isWorking();
 }
