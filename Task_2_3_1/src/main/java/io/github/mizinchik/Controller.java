10bd0914de18cbccbbed168cfcdfa10f247028aa
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2023-05-08 15:12:53 +0700

    Add help

--- Task_2_3_1/src/main/java/io/github/mizinchik/Controller.java
+++ Task_2_3_1/src/main/java/io/github/mizinchik/Controller.java
@@ -1,10 +1,28 @@
 package io.github.mizinchik;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public abstract class Controller {
     @FXML
-    private void showHelp() {
+    private MenuBar menuBar;
 
+    @FXML
+    private void showHelp(ActionEvent event) {
+        final Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(menuBar.getScene().getWindow());
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.getChildren().add(new Text("WASD/←↑↓→ to move" + System.lineSeparator() +
+                "please close this game it's crap"));
+        Scene dialogScene = new Scene(dialogVbox, 300, 200);
+        dialog.setScene(dialogScene);
+        dialog.show();
     }
 }
