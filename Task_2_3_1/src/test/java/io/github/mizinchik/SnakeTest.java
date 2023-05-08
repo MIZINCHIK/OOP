4034273b10e314cfea69f8609f333067d9f72f21
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2023-05-08 23:13:57 +0700

    Add javadoc to test class

--- Task_2_3_1/src/test/java/io/github/mizinchik/SnakeTest.java
+++ Task_2_3_1/src/test/java/io/github/mizinchik/SnakeTest.java
@@ -4,7 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Empty class to pass reviewdog.
+ *
+ * @author MIZINCHIK
+ */
 public class SnakeTest {
+    /**
+     * Empty test to pass reviewdog.
+     */
     @Test
     public void test() {
         assertTrue(true);
