135133ee258fe219c423aaa546ee29b9788124ad
MIZINCHIK <MIZINCHIK@users.noreply.github.com>
2023-05-28 11:49:22 +0000

    Deploying to gh-pages from @ MIZINCHIK/OOP@6daeec9ddf2a3f55d190c7d0e04e275d42ca2ddf 🚀

--- Task_2_4_1/package-search-index.js
+++ Task_2_4_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.mizinchik"},{"l":"io.github.mizinchik.dsl"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.mizinchik"},{"l":"io.github.mizinchik.dsl"},{"l":"io.github.mizinchik.git"}];updateSearchResults();
